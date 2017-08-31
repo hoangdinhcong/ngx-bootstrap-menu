a700c6cea0b9d95dc481daceeddc00ecb0cfa65d
MrHDC <hodico.iam@gmail.com>
2017-08-31 14:49:24 +0700

    add nav-menu-side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    $(document).ready(function () {
+      $('.navbar a.dropdown-toggle').on('click', function (e) {
+        let $el = $(this);
+        let $parent = $(this).offsetParent('.dropdown-menu');
+        $(this).parent('li').toggleClass('open');
+
+        if (!$parent.parent().hasClass('nav')) {
+          $el.next().css({ 'top': $el[0].offsetTop, 'left': $parent.outerWidth() - 4 });
+        }
+        $('.nav li.open').not($(this).parents('li')).removeClass('open');
+
+        return false;
+      });
+
+      $('#togglenavbar8').click(function () {
+        $('.nav-side-menu').toggleClass('open');
+      });
+    });
+  }
 }
